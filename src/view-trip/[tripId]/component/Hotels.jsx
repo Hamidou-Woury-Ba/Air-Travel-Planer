408acf19fd1061719c136f7dcc9575fb8cccfd81
Hamidou Woury Ba <hamidouwouryba@esp.sn>
2024-08-14 15:02:22 +0000

    Deconnexion avec google

--- src/view-trip/[tripId]/component/Hotels.jsx
+++ src/view-trip/[tripId]/component/Hotels.jsx
@@ -7,8 +7,8 @@ function Hotels({ trip }) {
             <h2 className='font-bold mt-5 text-xl'>Hotels Recommendation</h2>
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
                 {trip?.tripData?.hotels?.map((hotel, index) => (
-                    <Link to={'https://www.google.com/maps/search/?api=1&query=' + hotel?.hotelName + ", " + hotel?.hotelAddress} target='_blank'>
-                        <div key={index} className='hover:scale-105 transition-all cursor-pointer'>
+                    <Link to={'https://www.google.com/maps/search/?api=1&query=' + hotel?.hotelName + ", " + hotel?.hotelAddress} target='_blank' key={index}>
+                        <div className='hover:scale-105 transition-all cursor-pointer'>
                             <img src="/placeholder.jpg" alt="" className='rounded-xl' />
                             <div className='my-2 flex flex-col  gap-2'>
                                 <h2 className='font-medium'>{hotel?.hotelName}</h2>
--- src/view-trip/[tripId]/component/PlacesToVisit.jsx
+++ src/view-trip/[tripId]/component/PlacesToVisit.jsx
@@ -7,7 +7,7 @@ function PlacesToVisit({ trip }) {
       <h2 className='font-bold text-lg'>Places to visit</h2>
       <div>
         {trip?.tripData?.itinerary && Object.entries(trip.tripData.itinerary).map(([day, schedule], index) => (
-          <div className='mt-5'>
+          <div className='mt-5' key={index}>
             <h2 className='font-medium text-lg'>{day}</h2>
             <div className='grid md:grid-cols-2 gap-2'>
               {Object.entries(schedule).map(([timeOfDay, details], index) => (
